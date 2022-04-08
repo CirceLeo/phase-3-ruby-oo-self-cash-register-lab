bd749a3d758039f02defa1f22da75411215f1b0c
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-08 10:22:39 -0400

    commit tree 49d07b9347dd6818c9abc8d6a59be6463cfc2b52

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+    attr_accessor :total, :items, :discount
+    #attr_reader 
+    def initialize discount = 20
+        @total = 0
+        @discount = discount
+        @items = []
+    end
 
+    def add_item (title, price, quantity = 1)
+        @total = @total + (price * quantity)
+        quantity.times { @itmes.push title }
+    end
+
+    def apply_discount
+        if self.discount == 20
+            @total = @total * (discount / 100)
+            "After the discount, the total comes to $#{total}."
+        else "There is no discount to apply."
+        end
+        
+    end
+end
